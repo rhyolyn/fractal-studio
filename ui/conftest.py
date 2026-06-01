558fe38b76fbf63f9b3579517f9f8d989f7a55b3
rhyolyn <rhyolyn@gmail.com>
2026-05-31 20:42:46 -0700

    refactor: rename test_ui_redesign.py to test_ui.py

--- ui/conftest.py
+++ ui/conftest.py
@@ -6,7 +6,7 @@ try:
     import PySide6  # noqa: F401
 except ModuleNotFoundError:
     collect_ignore_glob = [
-        "tests/test_ui_redesign.py",
+        "tests/test_ui.py",
         "tests/test_package_layout_smoke.py",
         "tests/test_main_window_section_panel_states.py",
     ]
